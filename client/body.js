312fec357719d29551621184744cf30ca0ed4858
unknown <505335@8RMW00008404.ita.net.intra>
2015-05-25 16:39:13 +0200

    some refactoring

--- client/body.js
+++ client/body.js
@@ -1,91 +1,34 @@
 
-var paginationStep = 5; //5 elements for single page
-
 Template.body.rendered = function(){
-    Session.set("pageIndex",0);
-    geoLocalization.getLatLng();
+    if(!Session.get("friendList")) {
+        Session.set("friendList", []);
+    }
 };
 
 Template.body.helpers({
     yourEventList: function() {
         return Session.get("yourEventList");
     },
-    eventList: function() {
-        return Session.get("localEvents");
-    },
-    pageCount: function() {
-        return Session.get("localEvents").length;
-    },
-    pageIndex: function() {
-        return Session.get("pageIndex");
-    },
-    eventListPage: function() {
-        var idx = 0;
-        if(Session.get("pageIndex")) {
-            idx = Session.get("pageIndex");
-        } else {
-            Session.set("pageIndex", idx);
-        }
-
-        return Session.get("localEvents").slice(idx, idx+paginationStep);
-    },
-    isfriend: function() {
-        FB.api('/me/friends/'+this.id,//+ friend_id,
-               'get',
-               {access_token:Meteor.user().services.facebook.accessToken},
-               function(res){
-
-            if (res && !res.error) {
-                if(res.data) {
-                    console.log("is your friend? " + res.data);
-                    Session.set("isfriend",res.data);
-                }
-            }  else {
-                console.log("error occurred: " + res.error);
-                Session.set("isfriend",false);
-            }
-        });
-
-        return Session.get("isfriend");
+    friendList: function() {
+        return Session.get("friendList");
     }
 });
 
 Template.body.events({
-    "click #checkFriends": function() {
+    "click #addfriend": function() {
+        var friendName = $('#addfriendin').val();
+        $('#addfriendin').val('');
 
-        for(index in Session.get("localEvents")) {
-            for(invIdx in Session.get("localEvents")[index].invited) {
-                var curr = Session.get("localEvents")[index].invited[invIdx];
-                console.log("check friendship " + Meteor.user().services.facebook.id +  "(you) and " + curr.id);
-                FB.api('/'+Meteor.user().services.facebook.id+'/friends/' + curr.id,
-                    'get', {
-                        access_token: Meteor.user().services.facebook.accessToken
-                    },
-                    function (res) {
+        if(friendName) {
+            var friendList = Session.get("friendList");
 
-                        if (res && !res.error) {
-                            if (res.data) {
-                                console.log("is your friend? " + res.data);
-                                Session.get("localEvents")[index].invited[invIdx].isfriend = res.data;
-                            }
-                        } else {
-                            console.log("error occurred: " + res.error);
-                            Session.get("localEvents")[index].invited[invIdx].isfriend = false;
-                        }
-                });
+            if(friendList.indexOf(friendName) < 0) {
+                friendList.push(friendName);
+                Session.set("friendList", friendList);
             }
         }
     },
-    "click #prevEvent": function() {
-        if( Session.get("pageIndex") > 0) {
-            Session.set("pageIndex", Session.get("pageIndex") - paginationStep);
-        }
-    },
-    "click #nextEvent": function() {
-        if( Session.get("pageIndex") <= Session.get("localEvents").length) {
-            Session.set("pageIndex", Session.get("pageIndex") + paginationStep);
-        }
-    },
+
     "click #yourEvents":function() {
         console.log("your events");
         FB.api('/me/events',
@@ -104,37 +47,6 @@ Template.body.events({
             }  else {
                 console.log("error occurred: " + res.error);
             }
-        });
-    },
-
-    "click #findEvents":function() {
-
-        var lat = Session.get("lat");
-        var lng = Session.get("lng");
-        var query = $('#query').val();
-
-        //FB.api('/search?q='+query+'&type=event&center='+lat+','+lng+'&distance=10000',
-        FB.api('/search?q='+query+'&type=event',
-              {access_token:Meteor.user().services.facebook.accessToken},
-              function(res){
-                var events= [];
-
-                if (res && !res.error) {
-                    res.data.forEach(function(element) {
-                        //console.log("current event: "+JSON.stringify(element));
-
-                        FB.api('/'+element.id+'/invited',function(response){
-                            element.invited= response.data;
-                            events.push(element);
-                            console.log("invitation list: " + JSON.stringify(element.invited));
-                            Session.set("localEvents",events);
-                        });
-
-                    });
-
-                } else {
-                    console.log("error occurred: " + JSON.stringify(res.error));
-                }
         });
     }
 });
---
+++ client/cercaEventi.js
@@ -0,0 +1,113 @@
+
+var paginationStep = 5; //5 elements for single page
+
+Template.body.rendered = function(){
+    Session.set("pageIndex",0);
+
+    geoLocalization.getLatLng();
+};
+
+Template.cercaeventi.helpers({
+
+    pageCount: function() {
+        return Session.get("localEvents").length;
+    },
+    pageIndex: function() {
+        return Session.get("pageIndex");
+    },
+    eventList: function() {
+        return Session.get("localEvents");
+    },
+    eventListPage: function() {
+        var idx = 0;
+        if(Session.get("pageIndex")) {
+            idx = Session.get("pageIndex");
+        } else {
+            Session.set("pageIndex", idx);
+        }
+
+        return Session.get("localEvents").slice(idx, idx+paginationStep);
+    }
+});
+
+
+Template.cercaeventi.events({
+    "click #prevEvent": function() {
+        if( Session.get("pageIndex") > 0) {
+            Session.set("pageIndex", Session.get("pageIndex") - paginationStep);
+        }
+    },
+    "click #nextEvent": function() {
+        if( Session.get("pageIndex") <= Session.get("localEvents").length) {
+            Session.set("pageIndex", Session.get("pageIndex") + paginationStep);
+        }
+    },
+    "click #checkFriends": function() {
+        var friendList = Session.get("friendList");
+        var events = Session.get("localEvents");
+
+        for(index in events) {
+            for(invIdx in events[index].invited) {
+                var curr = events[index].invited[invIdx];
+               // console.log("check friendship " + Meteor.user().services.facebook.id +  "(you) and " + curr.id);
+                if(friendList.indexOf(curr.name) >= 0) {
+                    curr.isfriend = true;
+                    console.log("you have a friend " + JSON.stringify(curr));
+                } else {
+                    curr.isfriend = false;
+                }
+            }
+        }
+        Session.set("localEvents",events);
+    },
+    "click #findEvents":function() {
+
+        var friendList = Session.get("friendList");
+
+        var lat = Session.get("lat");
+        var lng = Session.get("lng");
+        var query = $('#query').val();
+        $('#query').val('');
+
+        FB.api('/search?q='+query+'&type=event&center='+lat+','+lng+'&distance=5000',
+              {access_token:Meteor.user().services.facebook.accessToken},
+              function(res){
+                var events= [];
+
+                if (res && !res.error) {
+                    res.data.forEach(function(element) {
+                        //console.log("current event: "+JSON.stringify(element));
+
+                        FB.api('/'+element.id+'/invited',
+                               'get',
+                               {access_token:Meteor.user().services.facebook.accessToken},
+                               function(response){
+                            if (response && !response.error) {
+                                element.invited = response.data;
+
+                                for(index in  element.invited) {
+                                    if(friendList.indexOf(element.invited[index].name) >= 0) {
+                                        element.invited[index].isfriend = true;
+                                        console.log("you have a friend " + JSON.stringify(element.invited[index].name));
+                                    } else {
+                                        element.invited[index].isfriend = false;
+                                    }
+                                }
+
+                                events.push(element);
+                                //console.log("invitation list: " + JSON.stringify(element.invited));
+                                Session.set("localEvents",events);
+                            } else {
+                                console.log("error in retrieving invitation list for event " + element.id + ". Error " + JSON.stringify(response.error));
+                            }
+                        });
+
+                    });
+
+                } else {
+                    console.log("error occurred: " + JSON.stringify(res.error));
+                }
+        });
+    }
+
+});
