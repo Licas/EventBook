773af73aa94ced392a6bc3c1b9a05c02890eac67
Manuel Morini <morinimanuel@gmail.com>
2015-05-26 00:34:14 +0200

    added routing

---
+++ client/router.js
@@ -0,0 +1,23 @@
+Router.map(function(){
+    this.route('userfriends', {
+        path:'/userfriends'
+    });
+});
+
+Router.map(function(){
+    this.route('userevents', {
+        path:'/userevents'
+    });
+});
+
+Router.map(function(){
+    this.route('cercaeventi', {
+        path:'/findevents'
+    });
+});
+
+Router.map(function(){
+    this.route('home', {
+        path:'/'
+    });
+});
